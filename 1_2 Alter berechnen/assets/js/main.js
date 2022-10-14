7cc9f160b3561238cad649cd7c0fe26fc8279bbf
sofia28304 <sofia.vogelhaupt@yahoo.de>
2022-10-14 09:13:09 +0200

    korrektur

--- 1_2 Alter berechnen/assets/js/main.js
+++ 1_2 Alter berechnen/assets/js/main.js
@@ -1,13 +1,19 @@
-function berechneAlter(event) {
-    // Website Reload wird verhindert
+// function berechneAlter(event) {
+//     // Website Reload wird verhindert
+//     event.preventDefault();
+
+//     const geburtsJahr = document.getElementById("Geburtsjahr").value - 2022;
+
+//     // spricht das element mit der Id "result" und beschreibt diese mit Inhalt aus Variable number.
+//     document.getElementById("result").innerHTML = berechnung;
+
+// }
+
+// Hier nochmal meine Avriablen Namen anpassen!|
+function AlterBerechnen(event) {
     event.preventDefault();
-    const geburtsJahr = document.getElementById("geburtsjahr").value;
 
-    let berechnung = 2022 - geburtsJahr;
+    const eingabeZahl = document.getElementById("Geburtsjahr").value;
 
-    // spricht das element mit der Id "result" und beschreibt diese mit Inhalt aus Variable number.
-    document.getElementById("result").innerHTML = berechnung;
-
+    document.getElementById("output").innerHTML = 2022 - eingabeZahl
 }
-
-// Hier nochmal meine Avriablen Namen anpassen!
